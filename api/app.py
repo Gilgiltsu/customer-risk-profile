f55a29ce5ec700f242299a202277cb4e3ef69af1
Gilgiltsu <telli.gilles@gmail.com>
2025-02-12 12:43:29 +0100

    requirements maj

--- api/app.py
+++ api/app.py
@@ -2,19 +2,34 @@ from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
 import os
+import zipfile
 
 # Initialiser l'application Flask
 app = Flask(__name__)
 
 def load_model():
     """Charge automatiquement le fichier .pkl dans le répertoire courant."""
-    pkl_files = [f for f in os.listdir('.') if f.endswith('.pkl')]
-    if not pkl_files:
-        raise FileNotFoundError("Aucun fichier .pkl trouvé dans le répertoire courant.")
-    # Charger le premier fichier .pkl trouvé
-    model_filename = pkl_files[0]
-    print(f"Chargement du modèle depuis {model_filename}")
-    return joblib.load(model_filename)
+    # Trouver le fichier .zip dans le répertoire src
+    zip_file = [f for f in os.listdir('src') if f.endswith('.zip')]
+    if not zip_file:
+        raise FileNotFoundError("Aucun fichier .zip trouvé dans le répertoire courant.")
+
+    zip_file_path = os.path.join('src', zip_file[0])
+
+    # Extraire le contenu du fichier ZIP
+    with zipfile.ZipFile(zip_file_path, 'r') as zipf:
+        zipf.extractall("extracted_model")
+
+    # Trouver le fichier .pkl extrait
+    pkl_file = [f for f in os.listdir('extracted_model') if f.endswith('.pkl')]
+    if not pkl_file:
+        raise FileNotFoundError("Aucun fichier .pkl trouvé dans l'archive ZIP.")
+
+    pkl_file_path = os.path.join('extracted_model', pkl_file[0])
+
+    # Charger le modèle
+    print(f"Chargement du modèle depuis {pkl_file_path}")
+    return joblib.load(pkl_file_path)
 
 # Charger le modèle
 model = load_model()
